624a30fba1fba9625cf9eeebdd2275813a7a06e8
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-05-09 20:56:37 +0500

    Solved

---
+++ search_insert_position.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target not in nums:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
+        else:
+            return nums.index(target)
+obj = Solution()
+print(obj.searchInsert([1,3,5,6],5))
+        
