d5e91410c0c0e2b39204183f50c8f173a91f238e
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-05-26 07:06:00 +0500

    Done

---
+++ single_number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        new_list = [i for i in nums if nums.count(i)==1]
+        return new_list[0]
+
+obj = Solution()
+print(obj.singleNumber([4,1,2,1,2]))
