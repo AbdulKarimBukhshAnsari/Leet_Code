315ab6b6d48f17a628024e821d13bfdc5a881174
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-05-26 07:10:29 +0500

    Done

---
+++ sqrt.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(x**0.5)
+    
+obj = Solution()
+print(obj.mySqrt(8))
