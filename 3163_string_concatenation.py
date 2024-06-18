8987ef744c7c4f807b12bf204e45af0927e62561
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-06-18 12:38:30 +0500

    Done

---
+++ 3163_string_concatenation.py
@@ -0,0 +1,5 @@
+print(3+2%7)
+
+s = "abcabcbb"
+nest = s[0:3].index(s[3])
+print(nest)
--- 826_most_profit.py
+++ 826_most_profit.py
@@ -1,12 +1,12 @@
 class Solution:
-    def maxProfitAssignment(self, difficulty: list[int], profit: list[int], worker: list[int]) -> int:
-        jobs = sorted(zip(difficulty,profit)) 
-        worker.sort()
-        i , max_profit ,result = 0,0,0
-        for ability in worker:
-            if i < len(jobs) and ability<=jobs[i][0]:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        jobs = sorted(zip(difficulty,profit)) # making a tuple to store the difficulty along with its profit 
+        worker.sort() #sorting the work list to ensure the efficiency 
+        i , max_profit ,result = 0,0,0 
+        for ability in worker: #iterating through worker list
+            while  i < len(jobs) and jobs[i][0]<=ability: #checking whether the end of the list and comparing that the difficulty is less thn to worker ability
                 max_profit = max(max_profit,jobs[i][1])
-            i+=1
+                i+=1
             result+=max_profit
         return result
-
+    
